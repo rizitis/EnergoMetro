2105cc0fdc2e1d2f9abd550fa40ffe32d741494e
Anagnostakis Ioannis <elahistos@yahoo.gr>
2023-12-26 19:54:29 +0200

    update

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in green.md blue.md yellow.md orange.md; do
+for file in EnergoMetro_LICENSE.md green.md blue.md yellow.md orange.md; do
     output="${file%.md}.pdf"
     pandoc "$file" -o "$output" --pdf-engine=xelatex -V mainfont="DejaVu Serif"
 done
