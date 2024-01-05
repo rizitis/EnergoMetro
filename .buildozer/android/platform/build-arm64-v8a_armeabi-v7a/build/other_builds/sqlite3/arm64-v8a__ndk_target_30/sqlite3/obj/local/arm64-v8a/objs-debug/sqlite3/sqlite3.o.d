fa3f4d392887106f65f0f229dd1e300f4e6c05fa
Anagnostakis Ioannis <elahistos@yahoo.gr>
2024-01-05 21:02:37 +0200

    .

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/arm64-v8a__ndk_target_30/sqlite3/obj/local/arm64-v8a/objs-debug/sqlite3/sqlite3.o.d
+++
@@ -1,2 +0,0 @@
-/home/omen/GITHUB/SofoRevma/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/arm64-v8a__ndk_target_30/sqlite3/obj/local/arm64-v8a/objs-debug/sqlite3/sqlite3.o: \
-  /home/omen/GITHUB/SofoRevma/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/arm64-v8a__ndk_target_30/sqlite3/jni/../sqlite3.c
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/armeabi-v7a__ndk_target_30/sqlite3/obj/local/armeabi-v7a/objs-debug/sqlite3/sqlite3.o.d
+++
@@ -1,2 +0,0 @@
-/home/omen/GITHUB/SofoRevma/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/armeabi-v7a__ndk_target_30/sqlite3/obj/local/armeabi-v7a/objs-debug/sqlite3/sqlite3.o: \
-  /home/omen/GITHUB/SofoRevma/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/armeabi-v7a__ndk_target_30/sqlite3/jni/../sqlite3.c
